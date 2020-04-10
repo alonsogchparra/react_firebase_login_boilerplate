b9f6551b6ebac742488c117641e794aad7d97e28
Alonso Parra <alonsogparra@gmail.com>
2020-04-10 03:37:46 -0400

    actionTypes added

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,5 @@
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+export const SIGNOUT_SUCCESS = 'SIGNOUT_SUCCESS';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
